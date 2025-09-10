73a8641d588302c7020000c80bfa9cd8aa257165
andrewb8614 <andrewb8614@gmail.com>
2025-09-10 17:49:15 -0600

    week 4 wed hw2

--- Week4/src/GradeCalculator.java
+++ Week4/src/GradeCalculator.java
@@ -1,68 +1,51 @@
 import java.util.Scanner;
 
-public class GradeCalculatorSolo {
+public class GradeCalculator {
 
 	public static void main(String[] args) {
 		System.out.println("Please enter your grade.");
-		while (true)	{
-		
+		while (true) {
+
 			Scanner input = new Scanner(System.in);
-		
-	
+
 			boolean validGrade = false;
 			while (!validGrade) {
 				float gradeInput = input.nextFloat();
-	
+
 				if (gradeInput >= 0) {
-					validGrade=true;
-	
-					
-					if (gradeInput >= 0 && gradeInput <60) {
+					validGrade = true;
+
+					if (gradeInput >= 0 && gradeInput < 60) {
 						System.out.println("your grade is F");
-					}
-					else if (gradeInput >= 60 && gradeInput <67) {
+					} else if (gradeInput >= 60 && gradeInput < 67) {
 						System.out.println("your grade is D");
-					}
-					else if (gradeInput >= 67 && gradeInput <69) {
+					} else if (gradeInput >= 67 && gradeInput < 69) {
 						System.out.println("your grade is D+");
-					}
-					else if (gradeInput >= 69 && gradeInput <72) {
+					} else if (gradeInput >= 69 && gradeInput < 72) {
 						System.out.println("your grade is C-");
-					}
-					else if (gradeInput >= 72 && gradeInput <77) {
+					} else if (gradeInput >= 72 && gradeInput < 77) {
 						System.out.println("your grade is C");
-					}
-					else if (gradeInput >= 77 && gradeInput <79) {
+					} else if (gradeInput >= 77 && gradeInput < 79) {
 						System.out.println("your grade is C+");
-					}
-					else if (gradeInput >= 79 && gradeInput <82) {
+					} else if (gradeInput >= 79 && gradeInput < 82) {
 						System.out.println("your grade is B-");
-					}
-					else if (gradeInput >= 82 && gradeInput <87) {
+					} else if (gradeInput >= 82 && gradeInput < 87) {
 						System.out.println("your grade is B");
-					}
-					else if (gradeInput >= 87 && gradeInput <89) {
+					} else if (gradeInput >= 87 && gradeInput < 89) {
 						System.out.println("your grade is B+");
-					}
-					else if (gradeInput >= 89 && gradeInput <92) {
+					} else if (gradeInput >= 89 && gradeInput < 92) {
 						System.out.println("your grade is A-");
-					}
-					else if (gradeInput >= 92) {
+					} else if (gradeInput >= 92) {
 						System.out.println("your grade is A");
 					}
-	
-				}
-				else
-				{
+
+				} else {
 					System.out.printf("Please input a valid grade.");
-	
+
 				}
 				System.out.println("If you'd like to check again, please enter your grade here.");
 			}
 		}
 	}
-
-
-	
 
 }
